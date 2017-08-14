a83b8287be04ead5d9f88e5d26ec19ba46b085e4
ame <qqqq1242380113@gmail.com>
2017-08-14 16:46:11 +0800

    http://wiki.jikexueyuan.com/project/mybatis-in-action/mybatis-and-spring.html

---
+++ src_user/com/yihaomen/mybatis/inter/IUserOperation.java
@@ -0,0 +1,14 @@
+package com.yihaomen.mybatis.inter;
+import java.util.List;
+
+import com.yihaomen.mybatis.model.Article;
+import com.yihaomen.mybatis.model.User;
+
+public interface IUserOperation {    
+    public User selectUserByID(int id);
+    public List selectUsers(String userName);
+    public void addUser(User user);
+    public void updateUser(User user);
+    public void deleteUser(int id);
+    public List<Article> getUserArticles(int id);
+}
